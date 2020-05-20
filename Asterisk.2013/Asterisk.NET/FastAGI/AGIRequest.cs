27fc34c6c9739e818124897c9157cc5c7d39f162
Jonas Swiatek <jswiatek@telzio.com>
2020-05-20 14:11:12 +0200

    Fixed bug

--- Asterisk.2013/Asterisk.NET/FastAGI/AGIRequest.cs
+++ Asterisk.2013/Asterisk.NET/FastAGI/AGIRequest.cs
@@ -328,7 +328,7 @@ namespace AsterNET.FastAGI
         /// </summary>
         public string AccountCode
         {
-            get { return request["accountCode"]; }
+            get { return request["accountcode"]; }
         }
 
         #endregion
