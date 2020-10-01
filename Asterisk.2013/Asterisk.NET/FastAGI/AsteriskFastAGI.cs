52763d025eb64485849ad5abfa8ab8fe1dfed1a9
Jonas Swiatek <jswiatek@telzio.com>
2020-10-01 15:41:16 +0200

    Enable socket keepalive

--- Asterisk.2013/Asterisk.NET/FastAGI/AsteriskFastAGI.cs
+++ Asterisk.2013/Asterisk.NET/FastAGI/AsteriskFastAGI.cs
@@ -1,5 +1,8 @@
+using System;
 using System.IO;
 using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
 using AsterNET.FastAGI.MappingStrategies;
 using AsterNET.IO;
@@ -252,6 +255,26 @@ namespace AsterNET.FastAGI
 #if LOGGER
                     logger.Info("Received connection.");
 #endif
+                    //Set keepalive
+                    // Get the size of the uint to use to back the byte array
+                    int size = Marshal.SizeOf((uint)0);
+
+                    // Create the byte array
+                    byte[] keepAlive = new byte[size * 3];
+
+                    // Pack the byte array:
+                    // Turn keepalive on
+                    Buffer.BlockCopy(BitConverter.GetBytes((uint)1), 0, keepAlive, 0, size);
+
+                    // Set amount of time without activity before sending a keepalive to 5 seconds
+                    Buffer.BlockCopy(BitConverter.GetBytes((uint)300000), 0, keepAlive, size, size);
+
+                    // Set keepalive interval to 5 seconds
+                    Buffer.BlockCopy(BitConverter.GetBytes((uint)300000), 0, keepAlive, size * 2, size);
+
+                    // Set the keep-alive settings on the underlying Socket
+                    socket.TcpClient.Client.IOControl(IOControlCode.KeepAliveValues, keepAlive, null);
+
                     var connectionHandler = new AGIConnectionHandler(socket, mappingStrategy, SC511_CAUSES_EXCEPTION,
                         SCHANGUP_CAUSES_EXCEPTION);
                     pool.AddJob(connectionHandler);
